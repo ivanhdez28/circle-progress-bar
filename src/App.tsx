ec335aba510c82aaa41f21f84d7dcfd3f6fc193b
Ivan Hernandez <ivan.hdez.e@gmail.com>
2022-01-10 10:56:06 -0600

    circle progress bar playground

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,89 @@
-import { useCallback, useEffect, useState } from "react";
+import { Component, useCallback, useEffect, useState } from "react";
 import "./styles.css";
 
 const circleConfig = {
   viewBox: "0 0 38 38",
   x: "19",
   y: "19",
-  radio: "15"
+  radio: "15",
 };
 
 // const percentage = 50;
-const CircleProgressBar = ({ percentage, direction = "clockwise" }) => {
-  const dir = direction === "clockwise" ? 1 : -1;
-  const transform = `rotate(-90deg) scale(1, ${dir})`;
+const CircleProgressBar = ({
+  percentage,
+}: {
+  percentage: number;
+  direction?: string;
+}): JSX.Element => {
+  //   const dir = direction === "clockwise" ? 1 : -1;
+  //   const transform = `rotate(-90deg) scale(1, ${dir})`;
+  const transform = `rotate(-90deg)`;
   return (
-    <figure>
-      <svg viewBox={circleConfig.viewBox}>
-        <circle
-          className="ring"
-          cx={circleConfig.x}
-          cy={circleConfig.y}
-          r={circleConfig.radio}
-          fill={"transparent"}
-          stroke="gray"
-          strokeDasharray={`${percentage} ${100 - percentage}`}
-          style={{
-            transform,
-            transformOrigin: "center center"
-          }}
-        />
-      </svg>
-    </figure>
+    <svg viewBox={circleConfig.viewBox}>
+      <circle
+        className="ring"
+        cx={circleConfig.x}
+        cy={circleConfig.y}
+        r={circleConfig.radio}
+        fill={"transparent"}
+        stroke="gray"
+        strokeDasharray={`${percentage} ${100 - percentage}`}
+        style={{
+          transform,
+          transformOrigin: "center center",
+        }}
+      />
+    </svg>
   );
 };
 
-const CountdownCircle = () => {
-  const [progressBar, setProgressBar] = useState(100);
+class CountdownCircle extends Component<
+  { duration?: number },
+  { progressBar: number }
+> {
+  state = {
+    progressBar: 4000,
+  };
+
+  componentDidMount = () => {
+    // const { duration } = this.props;
+    const time = new Date();
+    const t = setInterval(() => {
+      if (this.state.progressBar > 0) {
+        this.setState((prevState) => {
+          return {
+            progressBar: prevState.progressBar - 4,
+          };
+        });
+      } else {
+        clearInterval(t);
+        const end = new Date();
+        console.log("time diff", end.valueOf() - time.valueOf());
+      }
+    }, 1);
+  };
+
+  render = () => {
+    const { progressBar } = this.state;
+    return <CircleProgressBar percentage={progressBar / 40} />;
+  };
+}
+
+const CountdownCircleF = () => {
+  const [progressBar, setProgressBar] = useState(4000);
 
   useEffect(() => {
-    setInterval(
-      (progress) => {
-        if (progress > 0) {
-          console.log("set progress bar called", progress);
-          // setProgressBar(progress - 0.025);
-        }
-      },
-      1,
-      progressBar
-    );
-  }, [progressBar, setProgressBar]);
+    const t = setInterval(() => {
+      if (progressBar > 0) {
+        console.log("set progress bar");
+        setProgressBar((prevState) => {
+          return prevState - 4;
+        });
+      } else {
+        clearInterval(t);
+      }
+    }, 1);
+  }, []);
 
   return <CircleProgressBar percentage={progressBar} />;
 };
@@ -57,7 +93,23 @@ export default function App() {
     <div className="App">
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
-      <CountdownCircle />
+      <div
+        style={{
+          width: "300px",
+          height: "300px",
+        }}
+      >
+        <CountdownCircleF />
+      </div>
+      <div
+        style={{
+          width: "300px",
+          height: "300px",
+        }}
+      >
+        <CountdownCircle />
+      </div>
+      <CircleProgressBar percentage={10} />
     </div>
   );
 }
